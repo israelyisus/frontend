9bae8c2afee1ce0fe5891dbcfd88f487a28669ea
israelyisus <alh.guzman@duocuc.cl>
2026-06-24 14:16:48 -0400

    Cambio de color ROJO

--- src/componentes/Layouts/Navbar.jsx
+++ src/componentes/Layouts/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="rounded-xl w-[250px] min-h-[880px] bg-teal-600 text-white sticky top-0 p-4 m-4">
+    <nav className="rounded-xl w-[250px] min-h-[880px] bg-red-600 text-white sticky top-0 p-4 m-4">
       <h2 className="text-xl font-bold mb-8">Despacho Dashboard</h2>
       <ul className="space-y-3">
         <li>
